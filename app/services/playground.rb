cc94f66fbc300ae814a81f26bc2aa12b07b07cf9
chizhikov <chiibis@yandex.ru>
2020-11-08 20:22:34 +0300

    Batch import but bus_services is empty

--- app/services/playground.rb
+++ app/services/playground.rb
@@ -9,6 +9,14 @@ class Playground
 
     trips_count = 0
 
+    @services_cache = {}
+    @services_arr = []
+
+    @cities_cache = {}
+    @cities_arr = []
+
+    clear_db
+
     time = Benchmark.realtime do
       trips_count = json_stream_read
     end
@@ -21,16 +29,116 @@ class Playground
     puts "", "### <- END"
   end
 
+  def clear_db
+    City.delete_all
+    Bus.delete_all
+    Service.delete_all
+    Trip.delete_all
+
+    ActiveRecord::Base.connection.execute('delete from buses_services;')
+  end
+
+  def create_single_trip(trip)
+    from = City.find_or_create_by(name: trip['from'])
+    to = City.find_or_create_by(name: trip['to'])
+
+    services = []
+
+    trip['bus']['services'].each do |service|
+      s = Service.find_or_create_by(name: service)
+      services << s
+    end
+
+    bus = Bus.find_or_create_by(number: trip['bus']['number'])
+    bus.update(model: trip['bus']['model'], services: services)
+
+    trip_params = {
+      from: from,
+      to: to,
+      bus: bus,
+      start_time: trip['start_time'],
+      duration_minutes: trip['duration_minutes'],
+      price_cents: trip['price_cents']
+    }
+
+    Trip.new(trip_params)
+  end
+
+  def cached_service(service_name)
+    service = @services_cache[service_name]
+
+    unless service.present?
+      service = Service.new(name: service_name)
+      @services_cache[service_name] = service
+      @services_arr << service
+    end
+
+    service
+  end
+
+  def cached_city(city_name)
+    city = @cities_cache[city_name]
+
+    unless city.present?
+      city = City.new(name: city_name)
+      @cities_cache[city_name] = city
+      @cities_arr << city
+    end
+
+    city
+  end
+
   def json_stream_read
     counter = 0
     file_stream = File.open(full_path, 'r')
 
     streamer = Json::Streamer.parser(file_io: file_stream)
 
-    streamer.get(nesting_level: 1, symbolize_keys: true) do |trip|
+    trips = []
+    buses = []
+
+    streamer.get(nesting_level: 1, symbolize_keys: false) do |trip|
       counter += 1
-      puts "single trip #{trip}", "" if counter <= 3
+      next if counter > 1
+
+      from = cached_city(trip['from'])
+      to = cached_city(trip['to'])
+
+      bus_services = []
+      bus_node = trip['bus']
+
+      bus_node['services'].each do |service|
+        s = cached_service(service)
+        bus_services << s
+      end
+
+      # bus_key = bus_node['model'] + bus_node['number']
+      bus_params = {
+        number: bus_node['number'],
+        model: bus_node['model'],
+        services: bus_services
+      }
+
+      bus = Bus.new(bus_params)
+   
+      trip_params = {
+        from: from,
+        to: to,
+        bus: bus,
+        start_time: trip['start_time'],
+        duration_minutes: trip['duration_minutes'],
+        price_cents: trip['price_cents']
+      }
+
+      trips << Trip.new(trip_params)
+      buses << bus
+
     end
+
+    City.import(@cities_arr)
+    Service.import(@services_arr)
+    Bus.import buses, recursive: true
+    Trip.import(trips)
 
     counter
   end
